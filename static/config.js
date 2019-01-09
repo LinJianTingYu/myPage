722ee12f6faf139f82c2e12d88d9fa6e3149ab67
yujian <895144737@qq.com>
2019-01-09 20:51:05 +0800

    better

--- static/config.js
+++ static/config.js
@@ -1,7 +1,7 @@
-const config = {
-	baseURL: "https://evening-ocean-46832.herokuapp.com"
-}
-
 // const config = {
-// 	baseURL: "http://localhost:5001"
+// 	baseURL: "https://evening-ocean-46832.herokuapp.com"
 // }
+
+const config = {
+	baseURL: "http://localhost:5001"
+}
